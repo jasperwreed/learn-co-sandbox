c6f4758bea9a16d8080d7adbb42a19aa49981a50
Jasper Reed <jmixon.reed@gmail.com>
2021-01-08 21:12:13 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,9 @@
+3.times do 
+  puts "hi"
+end
+
+counter = 0
+until counter == 3
+  puts "hi"
+  counter += 1
+end
---
+++ learning-comments.rb
@@ -0,0 +1,14 @@
+# Don't do this:
+# puts (1 #+ 1)
+
+# Perform a constant expression evaluation
+3
+# Assign constant 3 to bare- word variable triangle_sides
+#`triangle_sides`
+
+# From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in
+the deep blue sea"
+puts "Jou to you and me"
